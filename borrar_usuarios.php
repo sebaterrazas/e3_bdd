178095693bd1167e03e87b7448ed2e76725722aa
SEBASTIAN TERRAZAS <sebaterrazas@uc.cl>
2022-12-01 16:04:05 -0300

    importar usuarios con PHP listo

--- borrar_usuarios.php
+++ borrar_usuarios.php
@@ -1,9 +1,14 @@
 <?php
     require ("config/conexion.php");
 
-    $query = "DROP TABLE usuarios; CREATE TABLE usuarios (id_usuario INT GENERATED ALWAYS AS IDENTITY, nombre_usuario VARCHAR (90) NOT NULL, contraseña VARCHAR (30) NOT NULL, tipo VARCHAR (20) NOT NULL);";
+    $query = "DROP TABLE usuarios;";
     $result = $db2 -> prepare($query);
     $result -> execute();
+
+    $query = "CREATE TABLE usuarios (id_usuario INT GENERATED ALWAYS AS IDENTITY, nombre_usuario VARCHAR (90) NOT NULL, contraseña VARCHAR (30) NOT NULL, tipo VARCHAR (20) NOT NULL);";
+    $result = $db2 -> prepare($query);
+    $result -> execute();
+
     header('Location: ' . 'usuarios.php', true, 303);
     die();
 ?>
