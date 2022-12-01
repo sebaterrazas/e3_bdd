d946cbc7c74f2b4c7184bfec8c13f159b11b22ed
SEBASTIAN TERRAZAS <sebaterrazas@uc.cl>
2022-12-01 14:42:20 -0300

    .

--- procedimientos_almacenados/importar_artistas.sql
+++ procedimientos_almacenados/importar_artistas.sql
@@ -2,11 +2,6 @@ CREATE OR REPLACE FUNCTION importar_artista(nombre varchar(90), contraseña_usua
 RETURNS BOOLEAN AS
 $$
 BEGIN
-    IF nombre NOT IN (SELECT nombre_usuario from usuarios) THEN
-        INSERT INTO usuarios (nombre_usuario, contraseña, tipo) VALUES (nombre, contraseña_usuario, 'artista');
-        RETURN TRUE;
-    ELSE 
-        RETURN FALSE;
-    END IF;
+    INSERT INTO usuarios (nombre_usuario, contraseña, tipo) VALUES (nombre, contraseña_usuario, 'artista');
 END
 $$ language plpgsql
