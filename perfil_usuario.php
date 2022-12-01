a8237a94c752c5518d354a26ed56c1f990eaae3b
diegotmz <diegotmz123@uc.cl>
2022-12-01 18:07:41 -0400

    .

--- perfil_usuario.php
+++ perfil_usuario.php
@@ -70,7 +70,7 @@ if ($tipo == 'productora') {
     echo "<div align='center' class = 'flex-container'>";
     echo "<h4>Eventos programados:</h4>";
     echo "<table>";
-    echo "<tr><th>Nombre</th><th>Fecha</th><th>Recinto</th><th>Artistas</th><th>Tour</th><th>Hospedaje</th><th>Traslado</th><th>Entradas</th></tr>";
+    echo "<tr><th>Nombre</th><th>Fecha</th><th>Recinto</th><th>Artista</th><th>Ciudad</th><th>País</th><th>Fecha</th><th>Productora</th></tr>";
     foreach ($eventos as $e) {
         echo "<tr><td>$e[1]</td> <td>$e[2]</td> <td>$e[3]</td> <td>$e[4]</td> <td>$e[5]</td> <td>$e[6]</td> <td>$e[7]</td> </tr>";
     }
