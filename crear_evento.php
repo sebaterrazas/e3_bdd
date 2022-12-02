96d3cf3c26466b58cd987d179f739bc5d750a795
SEBASTIAN TERRAZAS <sebaterrazas@uc.cl>
2022-12-02 17:06:44 -0300

    .

--- crear_evento.php
+++ crear_evento.php
@@ -9,27 +9,13 @@
     $fecha = $_POST['date'];
     $productor = $_SESSION['producer'];
 
-    echo $evento;
-    echo "---";
-    echo $recinto;
-    echo "---";
-    echo $ciudad;
-    echo "---";
-    echo $pais;
-    echo "---";
-    echo $fecha;
-    echo "---";
-    echo $productor;
-
     $query = "SELECT MAX(id_evento) FROM eventos;";
     $result = $db2 -> prepare($query);
     $result -> execute();
     $id = $result -> fetchAll();
     $id = $id[0][0] + 1;
-    echo "---";
-    echo $id;
 
-    $query = "INSERT INTO eventos (id_evento, nombre_evento, nombre_recinto, nombre_artista, ciudad_recinto, pais_recinto, fecha_evento, nombre_productora, estado) VALUES ($id, '$evento'::varchar, '$recinto'::varchar, '$ciudad'::varchar, '$pais'::varchar, '$fecha'::varchar, '$productor'::varchar, 'En espera');";
+    $query = "INSERT INTO eventos (id_evento, nombre_evento, nombre_recinto, nombre_artista, ciudad_recinto, pais_recinto, fecha_evento, nombre_productora, estado) VALUES ($id, '$evento'::varchar, '$recinto'::varchar, '$ciudad'::varchar, '$pais'::varchar, STR_TO_DATE('$fecha'::varchar, '%Y-%m-%d'), '$productor'::varchar, 'En espera');";
     $result = $db2 -> prepare($query);
     $result -> execute();
     /* header("Location: perfil_usuario.php");
