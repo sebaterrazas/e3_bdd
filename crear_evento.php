f8f78f036fea9f6df99e458d4bd75794a9e00e3b
SEBASTIAN TERRAZAS <sebaterrazas@uc.cl>
2022-12-02 17:22:27 -0300

    .

--- crear_evento.php
+++ crear_evento.php
@@ -4,6 +4,7 @@
 
     $evento = $_POST['event_name'];
     $recinto = $_POST['place_name'];
+    $artista = $_POST['nombre_artista'];
     $ciudad = $_POST['city'];
     $pais = $_POST['country'];
     $fecha = $_POST['date'];
@@ -15,7 +16,7 @@
     $id = $result -> fetchAll();
     $id = $id[0][0] + 1;
 
-    $query = "INSERT INTO eventos (id_evento, nombre_evento, nombre_recinto, nombre_artista, ciudad_recinto, pais_recinto, fecha_evento, nombre_productora, estado) VALUES ($id::int, '$evento'::varchar, '$recinto'::varchar, '$ciudad'::varchar, '$pais'::varchar, '$fecha'::varchar, '$productor'::varchar, 'a');";
+    $query = "INSERT INTO eventos (id_evento, nombre_evento, nombre_recinto, nombre_artista, ciudad_recinto, pais_recinto, fecha_evento, nombre_productora, estado) VALUES ($id::int, '$evento'::varchar, '$recinto'::varchar, '$artista'::varchar, '$ciudad'::varchar, '$pais'::varchar, '$fecha'::varchar, '$productor'::varchar, 'a');";
     $result = $db2 -> prepare($query);
     $result -> execute();
     header("Location: perfil_usuario.php");
